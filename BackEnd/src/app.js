8c352aa08f956df3007424187f0a287fe1e41410
Cesar Torres <cesargabrieltorresflorez@gmail.com>
2024-12-02 20:32:58 -0500

    GA1
    2

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
+import cors from 'cors'; // Importa el middleware CORS
 import authRoutes from './routes/auth.routes.js';
 import { requestLogger } from './middlewares/RequestLogger.js';
 import clientRoutes from './routes/client.routes.js';
@@ -11,6 +12,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 app.use(requestLogger);
 app.use('/api/auth', authRoutes);
 app.use('/api/client', clientRoutes);
